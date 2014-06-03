b447e6faa33c88f079a64477a375b99a7f01f018
Orson Peters <orsonpeters@gmail.com>
2014-06-03 08:05:19 +0200

    better build script, gitignore

--- build_mgw49.bat
+++ build_mgw49.bat
@@ -1,10 +1,18 @@
+@echo off
 setlocal enableextensions enabledelayedexpansion
 
+if [%1]==[] (
+    echo Usage: %0 ^<boost_dir^>
+    goto :eof
+)
+
+cd %1
 md stage\lib 2> nul
+md stage\include\boost 2> nul
 
-b2 --build-dir=build_gcc64 --stagedir=stage64 toolset=gcc variant=release link=static runtime-link=static address-model=64 -j 3
+b2 --build-dir=build_mgw49-64-s --stagedir=stage_mgw49-64-s toolset=gcc variant=release link=static runtime-link=static address-model=64 -j 3
 
-cd stage64\lib
+cd stage_mgw49-64-s\lib
 echo create libboost-mgw49-64-s.a >> all.mri
 for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
 echo save >> all.mri
@@ -14,9 +22,9 @@ del /Q all.mri
 move libboost-mgw49-64-s.a ..\..\stage\lib\
 cd ..\..
 
-b2 --build-dir=build_gcc32 --stagedir=stage32 toolset=gcc variant=release link=static runtime-link=static address-model=32 -j 3
+b2 --build-dir=build_mgw49-32-s --stagedir=stage_mgw49-32-s toolset=gcc variant=release link=static runtime-link=static address-model=32 -j 3
 
-cd stage32\lib
+cd stage_mgw49-32-s\lib
 echo create libboost-mgw49-32-s.a >> all.mri
 for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
 echo save >> all.mri
@@ -26,9 +34,9 @@ del /Q all.mri
 move libboost-mgw49-32-s.a ..\..\stage\lib\
 cd ..\..
 
-b2 --build-dir=build_gcc64_debug --stagedir=stage64_debug toolset=gcc variant=debug link=static runtime-link=static address-model=64 -j 3
+b2 --build-dir=build_mgw49-64-sd --stagedir=stage_mgw49-64-sd toolset=gcc variant=debug link=static runtime-link=static address-model=64 -j 3
 
-cd stage64_debug\lib
+cd stage_mgw49-64-sd\lib
 echo create libboost-mgw49-64-sd.a >> all.mri
 for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
 echo save >> all.mri
@@ -38,9 +46,9 @@ del /Q all.mri
 move libboost-mgw49-64-sd.a ..\..\stage\lib\
 cd ..\..
 
-b2 --build-dir=build_gcc32_debug --stagedir=stage32_debug toolset=gcc variant=debug link=static runtime-link=static address-model=32 -j 3
+b2 --build-dir=build_mgw49-32-sd --stagedir=stage_mgw49-32-sd toolset=gcc variant=debug link=static runtime-link=static address-model=32 -j 3
 
-cd stage32_debug\lib
+cd stage_mgw49-32-sd\lib
 echo create libboost-mgw49-32-sd.a >> all.mri
 for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
 echo save >> all.mri
@@ -49,3 +57,9 @@ ar -M < all.mri
 del /Q all.mri
 move libboost-mgw49-32-sd.a ..\..\stage\lib\
 cd ..\..
+
+xcopy /E /Y boost stage\include\boost
+
+cd ..
+
+:eof
