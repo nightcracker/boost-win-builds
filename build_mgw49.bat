f4234891db5d98ce92347d0cb7a44998d29790ca
Orson Peters <orsonpeters@gmail.com>
2014-06-03 07:29:14 +0200

    initial commit with mingw 4.9

---
+++ build_mgw49.bat
@@ -0,0 +1,51 @@
+setlocal enableextensions enabledelayedexpansion
+
+md stage\lib 2> nul
+
+b2 --build-dir=build_gcc64 --stagedir=stage64 toolset=gcc variant=release link=static runtime-link=static address-model=64 -j 3
+
+cd stage64\lib
+echo create libboost-mgw94-64-s.a >> all.mri
+for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
+echo save >> all.mri
+echo end >> all.mri
+ar -M < all.mri
+del /Q all.mri
+move libboost-mgw94-64-s.a ..\..\stage\lib\
+cd ..\..
+
+b2 --build-dir=build_gcc32 --stagedir=stage32 toolset=gcc variant=release link=static runtime-link=static address-model=32 -j 3
+
+cd stage32\lib
+echo create libboost-mgw94-32-s.a >> all.mri
+for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
+echo save >> all.mri
+echo end >> all.mri
+ar -M < all.mri
+del /Q all.mri
+move libboost-mgw94-32-s.a ..\..\stage\lib\
+cd ..\..
+
+b2 --build-dir=build_gcc64_debug --stagedir=stage64_debug toolset=gcc variant=debug link=static runtime-link=static address-model=64 -j 3
+
+cd stage64_debug\lib
+echo create libboost-mgw94-64-sd.a >> all.mri
+for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
+echo save >> all.mri
+echo end >> all.mri
+ar -M < all.mri
+del /Q all.mri
+move libboost-mgw94-64-sd.a ..\..\stage\lib\
+cd ..\..
+
+b2 --build-dir=build_gcc32_debug --stagedir=stage32_debug toolset=gcc variant=debug link=static runtime-link=static address-model=32 -j 3
+
+cd stage32_debug\lib
+echo create libboost-mgw94-32-sd.a >> all.mri
+for %%f in (libboost_*.a) do (echo addlib %%f >> all.mri)
+echo save >> all.mri
+echo end >> all.mri
+ar -M < all.mri
+del /Q all.mri
+move libboost-mgw94-32-sd.a ..\..\stage\lib\
+cd ..\..
